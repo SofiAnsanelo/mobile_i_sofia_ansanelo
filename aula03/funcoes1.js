91bbbc631b3eca2f17284829a4135505ac736f3a
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-03-14 10:59:12 -0300

    Aula sobre métodos e funções

---
+++ aula03/funcoes1.js
@@ -0,0 +1,15 @@
+function Soma () {
+    let n1 = 2;
+    let n2 = 6;
+
+    console.log(n1+n2)
+}
+
+Soma()
+
+function SomaComParametro (n1, n2) {
+    console.log(n1 + n2)
+}
+
+SomaComParametro(8, 10)
+SomaComParametro(50, 10)
---
+++ aula03/metodos.js
@@ -0,0 +1,12 @@
+let txt = "Hello amigos";
+
+console.log(txt.length); //mensura tamanho do texto
+
+console.log(txt.slice(6, 12)); //pega/corta uma parte do texto
+
+console.log(txt.replace("Hello", "Olá")); //substitui um ponto do texto por outra coisa
+
+let newTxt = txt.replace("Hello", "Olá");
+
+console.warn(newTxt.toLocaleUpperCase());
+
